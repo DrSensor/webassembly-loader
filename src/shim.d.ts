aa3e77684070476feb9d3b4d86a7d1ed74eca82c
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-10-07 06:47:13 +0700

    feat: add option to export wasm code as X
    X can be:
    🐣 'buffer' will export wasm code as Buffer class
    🐥 'module' or 'async-module' will export wasm code as WebAssembly.Module
    🐤 'instance' or 'async-instance' will export wasm code as WebAssembly.Instance
    🐔 'async' will "instantiate" wasm code asynchronously
    
    waiting for v17.1 to resolve d.ts issue when bundling
    👉 ezolenko/rollup-plugin-typescript2/issues/101 👈

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -10,7 +10,3 @@ declare module '@webpack-contrib/schema-utils' {
   const validate: (options: Options) => void;
   export default validate;
 }
-
-declare interface Options {
-  [key: string]: any;
-}
