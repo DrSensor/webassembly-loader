1f2558e6849c7022282a7228d567aa5686458f74
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-10-08 06:44:09 +0700

    test(lib): add test when used as a library
    😲 the new ts-jest now make jest configuration really simple

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,4 @@
 module.exports = {
 	testEnvironment: "node",
-	globals: {
-		"ts-jest": {
-			babelConfig: true
-		}
-	},
-	transform: {
-		"^.+\\.tsx?$": "ts-jest"
-	},
-	testPathIgnorePatterns: [
-		"<rootDir>/test/fixtures",
-		"<rootDir>/node_modules/",
-		"<rootDir>/packages/"
-	],
-	coveragePathIgnorePatterns: [
-		".*\\.d\\.ts",
-		"<rootDir>/node_modules/",
-		"<rootDir>/packages/"
-	],
-	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-	preset: "ts-jest",
-	testMatch: null
+	preset: "ts-jest/presets/js-with-ts"
 }
