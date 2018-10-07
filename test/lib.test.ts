3e8d6b54015fea4b7aaf1f702ce351fe4ab5eeb4
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-10-08 06:44:09 +0700

    test(lib): add test when used as a library
    😲 the new ts-jest now make jest configuration really simple

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,2 +1,58 @@
-// tslint:disable-next-line:no-empty
-describe.skip('When used as a Library', () => test('', () => {}));
+// tslint:disable:no-eval
+import { wasm2js } from '../src';
+import 'jest-extended';
+
+describe('When used as a Library', () => {
+  /** @see https://developer.mozilla.org/en-US/docs/WebAssembly/Understanding_the_text_format#The_simplest_module */
+  const wasmBuffer = Buffer.from([0x00, 0x61, 0x73, 0x6d, 0x01, 0, 0, 0]);
+
+  test('export as Buffer class', () => {
+    const code = wasm2js(wasmBuffer, 'buffer');
+    const exportedModule = eval(code);
+
+    expect(exportedModule).toBeInstanceOf(Buffer);
+  });
+
+  test('export as WebAssembly.Module', () => {
+    const code = wasm2js(wasmBuffer, 'module');
+    const exportedModule = eval(code);
+
+    expect(exportedModule).toBeInstanceOf(WebAssembly.Module);
+  });
+
+  test('export as WebAssembly.Instance', () => {
+    const code = wasm2js(wasmBuffer, 'instance');
+    const exportedModule = eval(code);
+
+    expect(exportedModule).toBeInstanceOf(WebAssembly.Instance);
+  });
+
+  test('export of WebAssembly.compile', () => {
+    const code = wasm2js(wasmBuffer, 'async-module');
+    const exportedModule = eval(code);
+
+    expect(exportedModule).toBeFunction();
+    expect(exportedModule()).resolves.toBeInstanceOf(WebAssembly.Module);
+  });
+
+  test('export of export of WebAssembly.instantiate(WebAssembly.Module)', () => {
+    const code = wasm2js(wasmBuffer, 'async-instance');
+    const exportedModule = eval(code);
+
+    expect(exportedModule).toBeFunction();
+    expect(exportedModule()).resolves.toBeInstanceOf(WebAssembly.Instance);
+  });
+
+  test('export of WebAssembly.instantiate(Buffer))', () => {
+    const code = wasm2js(wasmBuffer, 'async');
+    const exportedModule = eval(code);
+
+    expect(exportedModule).toBeFunction();
+    expect(exportedModule()).resolves.toEqual(
+      expect.objectContaining({
+        instance: expect.any(WebAssembly.Instance),
+        module: expect.any(WebAssembly.Module)
+      })
+    );
+  });
+});
