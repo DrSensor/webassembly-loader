b8464f10f9610b3337701177d775c4f444836379
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-10-14 18:34:07 +0700

    feat: produce error when source is incorrect
    incorrect source could be:
    🔸 invalid/not-compilable wasm binary
    🔸 wasm code over 4KB when using export mode: instance or module
    
    ⚠️ Rust v1.29 and nightly produce ~57KB wasm code, smoke-test need to lock on v1.28
    👉 https://developers.google.com/web/updates/2018/04/loading-wasm 👈

--- src/transform.ts
+++ src/transform.ts
@@ -1,15 +1,37 @@
 import './__global';
 import wrap from './wrapper';
 
+//#region helpers
+const is = (type: string) => ({
+  oneOf: (types: string[]) => types.some(_ => _ === type)
+});
+function panic(message: string, cb?: (msg: string) => void) {
+  if (cb) cb(message);
+  else throw new Error(message);
+}
+//#endregion
+
 /** Transform WebAssembly binary into JS module
  * @param source - WebAssembly Buffer
+ * @param exportType - what type of export you want to generate
+ * @param {Function} [errorCallback] - callback error in case you want to use your own error reporting
  * @return string of the code
  * @example const code = wasm2js(Buffer.from([0x00, 0x61, 0x73, 0x6d, 0x01, 0, 0, 0]), 'instance')
  */
 export default function(
   source: Buffer,
-  exportType: Export.Type
+  exportType: Export.Type,
+  errorCallback?: (errorMessage: string) => void
 ): string | never {
+  if (WebAssembly.validate(source) === false)
+    panic('Invalid WebAssembly file', errorCallback);
+
+  if (is(exportType).oneOf(['instance', 'module']) && source.byteLength > 4000)
+    panic(
+      `The buffer size is larger than 4KB. Consider using async-${exportType}`,
+      errorCallback
+    );
+
   switch (exportType) {
     case 'buffer':
       return wrap(source).asBuffer;
